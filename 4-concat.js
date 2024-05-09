c86904adfd40b9f8bde1f906964c5eafa7f7c7fe
NNunez <nathalynunezmedina@gmail.com>
2024-05-09 09:16:19 -0500

    fifth exercise

---
+++ 4-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function concat(first, second) {
+    if(first === undefined && second ===undefined) {
+        console.log(`${first} is ${second}`);
+    } else {
+        first = first;
+        second = second;
+        console.log(`${first} is ${second}`);
+    }
+}
+
+concat();
